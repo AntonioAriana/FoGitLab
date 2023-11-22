3bb4efef5625fbfff5d13bf865d3c79b4673e006
ARIANA ANTONIO ALBAÑIL <20301184@uttt.edu.mx>
2023-11-22 01:56:30 -0600

    Mi primer commit

---
+++ ANGULAR/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ANGULAR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
